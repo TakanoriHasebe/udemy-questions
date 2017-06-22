40a8c3e2781251c939694ac9fa2375dbaceba5c7
Takanori Hasebe <jimmyflyingstrat@gmail.com>
2017-06-22 14:25:45 +0900

    追加

---
+++ RecommedPersonMain/SwiftCollectionView1/ButtonCell.swift
@@ -0,0 +1,15 @@
+//
+//  ButtonCell.swift
+//  SwiftCollectionView1
+//
+//  Created by Takanori.H on 2017/06/07.
+//  Copyright © 2017年 Takanori.H. All rights reserved.
+//
+
+import UIKit
+
+class ButtonCell: UIButton {
+
+    
+
+}
